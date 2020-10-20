e80d86284284f6792172c6c3c7bdb801d28788c0
Krypaloski <72460403+Krypaloski@users.noreply.github.com>
2020-10-20 12:52:28 +0200

    Chrono
    ajout du chrono

--- graph5/main.c
+++ graph5/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <allegro.h>
+#include <time.h>
+#include <process.h>
+#include <winalleg.h>
+#include <windows.h>
 
 int main()
 {
@@ -12,13 +16,14 @@ int main()
     int x_perso = 1;
     int y_perso = 1;
     int delta_perso = 1;
+    int valeur = 0;
+    int couleur;
+    couleur=makecol(255,255,255);
+    BITMAP *image;
 
 
     Create(rectangle,1); //0 niveau vide 1 niveau basique, 2 niveau 2... >4 niveau aléatoire
     affichage(rectangle);
-
-
-
 
 
 
@@ -34,11 +39,24 @@ int main()
 
     AffichageAllegro(rectangle); // affichage de labyrinthe
 
+
+    //Chronometre
+    start :
+            /*printf("\n\nappuyez sur une touche pour lancer le chronometre.\n");
+            system("PAUSE");*/
+            repere1 :
+            system("cls");
+            textprintf_ex(screen,font,775,20,couleur,-1,"%03d",valeur);
+            valeur++;
+            Sleep(1000); //le prog attend une seconde
+            image=load_bitmap("image/brick grey.bmp",NULL);
+            blit(image,screen,0,0,775,0,image->w, image->h);
+            goto repere1; //renvoie au repere1 pour ajouter une seconde
+
     PersoAffichage(x_perso,y_perso);
 
     while (!key[KEY_ESC])
     {
-
         if (key[KEY_RIGHT]) x_perso = PersoDeplacementX(rectangle,x_perso, y_perso, delta_perso, 1,0);
         if (key[KEY_LEFT]) x_perso = PersoDeplacementX(rectangle,x_perso, y_perso, delta_perso, -1,0);
         if (key[KEY_UP]) y_perso = PersoDeplacementY(rectangle,x_perso, y_perso, delta_perso, 0,-1);
